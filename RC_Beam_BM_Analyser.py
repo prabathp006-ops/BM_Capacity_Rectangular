e9dc7b353083670a9bb1c402df8bffa84514261e
prabathp006-ops <prabath.p006@gmail.com>
2026-03-15 19:55:16 +0530

    Update RC_Beam_BM_Analyser.py

--- RC_Beam_BM_Analyser.py
+++ RC_Beam_BM_Analyser.py
@@ -337,7 +337,8 @@ beam_dim_1(width, depth)
 fract   = frac("\\pi", 4)
 str_eq  = r"{N_{bars}} \cdot \frac{\pi}{4} \cdot {d_{bar}}^{2}"
 numer_1 = f"{bar_nos} \cdot {fract} \cdot {bar_dia}^{2}"
-str_fun_3("Area of rebars, ", "A_{s}", str_eq, 0, numer_1, round(bar_area,2), "in^{2}")
+st.latex(fract)
+#str_fun_3("Area of rebars, ", "A_{s}", str_eq, 0, numer_1, round(bar_area,2), "in^{2}")
 #rein_prop_1(bar_nos, bar_dia, bar_area)
 #mat_prop_1(f_y, f_c, beta_1)
 #stress_block_b1(f_c, beta_1)
