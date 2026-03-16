a57e70b0e9ceb7790dbdf4ee7235df0c88ea124b
prabathp006-ops <prabath.p006@gmail.com>
2026-03-16 21:51:36 +0530

    Update RC_Beam_BM_Analyser.py

--- RC_Beam_BM_Analyser.py
+++ RC_Beam_BM_Analyser.py
@@ -342,7 +342,7 @@ string_r = "Area of rebars, "
 var_1    = "A_{s}"
 dim_unt  = r"{in^{2}}"
 st.latex(rf"\text{{{string_r}}} {var_1} \hspace{{{0}cm}} = {str_eq}\;\text{{{""}}}")
-st.latex(rf" = {numer_1}\;\text{{{dim_unt}}}")
+st.latex(rf" = {numer_1}\;{dim_unt}")
 #str_fun_3("Area of rebars, ", "A_{s}", str_eq, 0, numer_1, round(bar_area,2), "in^{2}")
 #rein_prop_1(bar_nos, bar_dia, bar_area)
 #mat_prop_1(f_y, f_c, beta_1)
